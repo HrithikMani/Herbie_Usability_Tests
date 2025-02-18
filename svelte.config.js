b4b47fb3a5e9017e50a61d74222cc3d797ae7ae9
hrithikmani <hrithik.mani2000@gmail.com>
2025-02-18 14:20:24 -0500

    fix svelete pretending

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,13 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      pages: 'build',  // Output directory
+      pages: 'build',
       assets: 'build',
-     fallback: 'index.html'
+      fallback: 'index.html'  // Allows refresh to work
     }),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/Herbie_Usability_Tests' : ''
-    }
+    },
+    appDir: '_app'
   }
 };
